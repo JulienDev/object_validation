80b3077ff6c9fa9c14b17ccc2e09393072330d5b
JulienDev <julienv@heetch.com>
2018-08-16 17:58:53 +0200

    Clean

--- src/Persons.kt
+++ src/Persons.kt
@@ -1,22 +1,13 @@
-import com.google.gson.Gson
 import java.io.File
-import kotlin.reflect.full.memberProperties
-import kotlin.reflect.jvm.javaField
-
-//ANNOTATION
-@Target(AnnotationTarget.FIELD)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class Mandatory
-
-//THROWABLE
-class EntityInvalid(message: String?) : Throwable(message)
+import com.google.gson.GsonBuilder
 
 //NETWORK MODEL
 data class PersonWS(
         val name: String,
         val age: Int,
         val description: String,
-        val shows: List<ShowWS>
+        val shows: List<ShowWS>,
+        val spouse: PersonWS
 )
 
 data class ShowWS(
@@ -35,66 +26,48 @@ data class PersonEntity(
 data class ShowEntity(
         val name: String,
         val channel: String
-) : EntityValidation
+)
 
-abstract class AbsMapper
-
-class PersonsMapper : AbsMapper() {
+class PersonsMapper {
 
     fun convert(personsWS: Array<PersonWS>): List<PersonEntity> {
         val persons = arrayListOf<PersonEntity>()
-        personsWS.forEach {personWS ->
+        personsWS.forEach { personWS ->
             try {
                 val personEntity = convert(personWS)
                 persons.add(personEntity)
-            } catch (e:Exception) {
-                println(e.message)
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
         }
         return persons
     }
 
-    fun convert(personWS: PersonWS): PersonEntity {
+    private fun convert(personWS: PersonWS): PersonEntity {
         val shows = arrayListOf<ShowEntity>()
-        personWS.shows.forEach {showWS ->
+        personWS.shows.forEach { showWS ->
             try {
                 val showEntity = convert(showWS)
                 shows.add(showEntity)
-            } catch (e:Exception) {
-                println(e.message)
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
         }
-        val personEntity = PersonEntity(personWS.name, personWS.age, personWS.description, shows)
-        return personEntity
+        return PersonEntity(personWS.name, personWS.age, personWS.description, shows)
     }
 
-    fun convert(WS: ShowWS): ShowEntity {
+    private fun convert(WS: ShowWS): ShowEntity {
         return ShowEntity(WS.name, WS.channel)
     }
 
 }
 
-interface EntityValidation {
-    @Throws(EntityInvalid::class)
-    fun validate() {
-        javaClass.kotlin.memberProperties.forEach {
-            val isMandatory = it.javaField?.isAnnotationPresent(Mandatory::class.java) ?: false
-            if (isMandatory) {
-                val value = it.get(this)
-                when (value) {
-                    is String -> if (value.isBlank()) throw EntityInvalid("${it.name} must not be empty")
-                }
-                println(value)
-            }
-        }
-    }
-}
-
 fun main(args: Array<String>) {
+    val gson = GsonBuilder().setPrettyPrinting().create()
     val fileContent = readFileAsLinesUsingReadLines("persons.json")
-    val personsWS = Gson().fromJson(fileContent, Array<PersonWS>::class.java)
+    val personsWS = gson.fromJson(fileContent, Array<PersonWS>::class.java)
     val personsEntity = PersonsMapper().convert(personsWS)
-    println(personsEntity)
+    println(gson.toJson(personsEntity))
 }
 
 //UTIL
